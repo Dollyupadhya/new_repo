d926d1602fa7d3da51c4331f5046ac901f28b328
Dolly <dollyup8july@gmail.com>
2018-03-27 12:07:37 +0530

    cmit

---
+++ src/collection/Product.java
@@ -0,0 +1,11 @@
+package collection;
+
+public class Product {
+	int pro_id;  
+	  String pro_name;  
+	  double pro_price;  
+	  Product(int pro_id,String pro_name,double pro_price){  
+	   this.pro_id=pro_id;  
+	   this.pro_name=pro_name;  
+	   this.pro_price=pro_price;}
+}
---
+++ src/collection/new_coll.java
@@ -0,0 +1,40 @@
+package collection;
+import java.util.*;
+
+
+public class new_coll {
+public static void main(String args[])
+{
+	Product s1=new Product(01,"ABC",10000);  
+	Product s2=new Product(02,"XYZ",2000);  
+	Product s3=new Product(03,"RST",25000);
+	Product s4=new Product(04,"PQR",209000);
+	Product s5=new Product(03,"RST",25000);
+LinkedList<Product> pro=new LinkedList<Product>();
+pro.add(s1);
+pro.add(s2);
+pro.add(s3);
+pro.add(1,s4);
+pro.add(s2);
+pro.add(s5);
+Iterator r=pro.iterator();
+while(r.hasNext())
+{
+	Product pr=(Product)r.next();
+System.out.println(pr.pro_id +" " +pr.pro_name +" " +pr.pro_price);
+}
+System.out.println("---------");
+HashSet<Product> hs=new HashSet<Product>();
+hs.add(s1);
+hs.add(s3);
+hs.add(s5);
+hs.add(s1);
+Iterator r1=hs.iterator();
+while(r1.hasNext())
+{
+	Product pr=(Product)r1.next();
+System.out.println(pr.pro_id +" " +pr.pro_name +" " +pr.pro_price);
+//change
+}
+}
+}
